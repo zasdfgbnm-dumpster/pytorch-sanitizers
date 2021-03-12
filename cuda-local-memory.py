8105ddb5afeb5111e1c7d8e197f915c55244fb4f
Xiang Gao <qasdfgtyuiop@gmail.com>
2021-03-11 16:38:29 -0800

    save

--- cuda-local-memory.py
+++ cuda-local-memory.py
@@ -5,6 +5,13 @@ import json
 import colorama
 import sys
 import os
+import shutil
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--demangle', action='store_true')
+args = parser.parse_args()
 
 colorama.init()
 
@@ -15,12 +22,14 @@ files = set(glob.glob('pytorch/aten/src/ATen/native/cuda/*.cu'))
 nvcc = 'nvcc'
 target = ['-dc', '-o', '/dev/null']
 sanitize = ['-Xptxas=-Werror', '-Xptxas=-warn-lmem-usage,-warn-spills']
-features = ['--extended-lambda', '--expt-relaxed-constexpr']
+features = ['--extended-lambda', '--expt-relaxed-constexpr', '--keep']
 archs = ['-gencode', 'arch=compute_70,code=sm_70']
 defs = ['-DCUDA_HAS_FP16=1', '-D__CUDA_NO_HALF_OPERATORS__', '-D__CUDA_NO_HALF_CONVERSIONS__', '-D__CUDA_NO_BFLOAT16_CONVERSIONS__', '-D__CUDA_NO_HALF2_OPERATORS__']
 includes = ['-Ipytorch', '-Ipytorch/aten/src/', '-Ipytorch/build', '-Ipytorch/build/aten/src', '-Ipytorch/build/caffe2/aten/src']
 if not os.path.isdir('/usr/local/cuda/include/cub'):
     includes.append('-Ipytorch/third_party/cub')
+
+keep_dir = 'keep'
 
 
 def get_nvcc_command(file):
@@ -50,7 +59,18 @@ async def demangle(symbol):
 async def get_function_name(error):
     error = error.split("'")
     assert len(error) == 3
-    return await demangle(error[1])
+    return await demangle(error[1]) if args.demangle else error[1]
+
+
+def save_or_remove_keep_files(original_filename, keep):
+    _, fn = os.path.split(original_filename)
+    pattern = fn[:-len('cu')] + '*'
+    files = glob.glob(pattern)
+    for f in files:
+        if keep:
+            shutil.move(f, keep_dir)
+        else:
+            os.remove(f)
 
 
 async def run_single(file):
@@ -67,15 +87,27 @@ async def run_single(file):
         if len(stderr) > 0:
             print(colorama.Fore.RED + 'FAIL:', file)
             errors[file] = stderr
+            save_or_remove_keep_files(file, True)
         else:
             print(colorama.Fore.MAGENTA + 'UNKNOWN:', file)
             print(stderr_)
+            save_or_remove_keep_files(file, False)
     else:
         print(colorama.Fore.GREEN + 'PASS:', file)
+        save_or_remove_keep_files(file, False)
+
+
+def rm_then_mkdir(path):
+    try:
+        shutil.rmtree(path)
+    except FileNotFoundError:
+        pass
+    os.makedirs(path)
 
 
 async def main():
     tasks = set()
+    rm_then_mkdir(keep_dir)
     while len(files) > 0:
         if len(tasks) < ncpus:
             f = next(iter(files))
